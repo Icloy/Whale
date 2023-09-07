67dc7d70f40228e1ff2a2fd7986b0e2a82f1c1c2
Icloy <70014902+Icloy@users.noreply.github.com>
2023-09-07 13:47:34 +0900

    Init

---
+++ Assets/02_Scripts/Manager/MainManager.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace farm
+{
+    public class MainManager : Singleton<MainManager>
+    {
+        public UIManager uiManager;
+
+    }
+}
---
+++ Assets/02_Scripts/Manager/UIManager.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace farm{
+
+    public class UIManager : MonoBehaviour
+    {
+
+    }
+}
