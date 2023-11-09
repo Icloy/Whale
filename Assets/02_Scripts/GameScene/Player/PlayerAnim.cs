dbc1dedcaf13d9c06ff27920f1b3db9bc13229a4
Yuhahak <dbwogkr1214@naver.com>
2023-11-09 10:37:11 +0900

    Update PlayerAnim.cs

--- Assets/02_Scripts/GameScene/Player/PlayerAnim.cs
+++ Assets/02_Scripts/GameScene/Player/PlayerAnim.cs
@@ -7,8 +7,12 @@ public class PlayerAnim : MonoBehaviour
     public Animator anim;
 
     float maxTime = 0.5f;
-    public float curTime = 0f; 
+    public float curTime = 0f;
 
+    private void Start()
+    {
+        StartCoroutine(Run());
+    }
 
     IEnumerator Run()
     {
